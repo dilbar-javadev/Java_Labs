27860638f244c2b3fd105564de97b872969c95dc
Dibar <dibux9@gmail.com>
2022-06-05 15:06:28 -0400

    0

--- src/lab07_ClassAndObject/TestCydeoStudentObject.java
+++ src/lab07_ClassAndObject/TestCydeoStudentObject.java
@@ -4,9 +4,23 @@ public class TestCydeoStudentObject {
 
     public static void main(String[] args) {
 
+        CydeoStudent Cydeostudent1 = new CydeoStudent();
+        Cydeostudent1.setInfo("Dilbar",29,'F',true,false, "JDB1", "Java",7);
+
+        System.out.println(Cydeostudent1);
+
+        System.out.println("-----------------------------------------------");
+
         CydeoStudent student1 = new CydeoStudent();
+        CydeoStudent student2 = student1;
+        CydeoStudent student3 = student2;
+
         student1.setInfo("Dilbar",29,'F',true,false, "JDB1", "Java",7);
 
+        student3.age = 35;
+
         System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
     }
 }
